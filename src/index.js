bcc7b1636979d82a037eb66d88da714aba0f8eb4
Farid Kurbanou <kurbanou.faridun@gmail.com>
2022-02-17 00:43:05 +0300

    ff

--- src/index.js
+++ src/index.js
@@ -36,37 +36,57 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
+const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+let result = ''
+let a = []
+let b
+let c = []
+let arrayFromExpr = []
 
-function decode(expr) {
-    // write your solution here
-    let result = ''
-    let arrayFromExpr = []
-    for (let i = 0; i < expr.length; i+=10) {
-        arrayFromExpr.push(expr.slice(i, i + 10))
+for (let i = 0; i < expr.length; i += 10) {
+    arrayFromExpr.push(expr.slice(i, i + 10))
+}
+console.log(arrayFromExpr)
+
+for (let i = 0; i < arrayFromExpr.length; i++ ) {
+
+    if(arrayFromExpr[i] !== '**********'){
+        a = arrayFromExpr[i].match(/(.{1,2})/gim) /*детаем массив разбивая элемент-строку на массив по 2 цифры */
+        console.log(a)
+        for(let i = 0; i< a.length; i++){
+            if(a[i] == '10'){b +='.'}
+            else if(a[i] == '11'){b +='-'}
+                c.push(b)
+        }
+        c.push(b)
+
+
     }
+    else{c.push('**********')}
 
-    for (let i = 0; i < arrayFromExpr.length; i++ ) {
-        
-        
-    }
+}
 
-   
+
+
+
+
+
     // function arrayFromeExpr(){
 
-        
+
     // }
 
     // function seachLetter(MORSE_TABLE, a){
     //     for (let key in MORSE_TABLE) {
-    //         if(key == a){result +=a}            
+    //         if(key == a){result +=a}
     //       }
     // }
 
 
 
     // return result
-}
 
-module.exports = {
-    decode
-}
+
+// module.exports = {
+//     decode
+// }
